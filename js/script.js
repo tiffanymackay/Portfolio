38f186acbbed03352508fda583280e2eec9318a8
tmackay1 <88056784+tiffanymackay@users.noreply.github.com>
2021-08-23 15:59:03 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+        //Back to top button
+        let myButton = document.getElementById("button");
+        let display = document.querySelector("#button");
+
+        //Function that removes display-button class when user scrolls down and adds when scrolls up
+        function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+           myButton.classList.remove("display-button");
+        } else {
+            myButton.classList.add("display-button");
+        }
+        }
+
+        //Invokes the above function
+        window.onscroll = function() {scrollFunction()};
+        
+        //Function that brings user back to top
+        function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        }
+
+        //Invokes the above function on button click
+        myButton.addEventListener("click", topFunction);
+        
