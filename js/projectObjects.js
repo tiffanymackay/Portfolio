e582961476adb77fb9031151d206a18251696c5a
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-08-26 21:00:48 -0700

    Update projectObjects.js

--- js/projectObjects.js
+++ js/projectObjects.js
@@ -1,11 +1,15 @@
 const projects = {
     responsiveRecipe : {
         name: `Responsive Recipe` ,
+        link: `<a href="https://tiffanymackay.github.io/responsive-recipe/index.html" target="_blank">Batter Chatter ↗️ </a>`,
         text: `A responsive web page for a Batter Chatter recipe from concept to delivery. I created napkin sketches using my digital tablet, a Figma mockup of a high fidelity wireframe, then coded using HTML and CSS in Visual Studio Code. I made it live by uploading to GitHub. For the responsiveness, I created different break points to implement media queries. The code is semantic and I use comments for improved organization. Flexbox and grid were both used for different sections of the page.`,
-        process: `💡 Process: Mood board > Napkin Sketches > Wireframe > Code > Validate > Launch `       
+        process: `💡 Process: Mood board > Napkin Sketches > Wireframe > Code > Validate > Launch `,
+        img1: `../images/recipe-images/recipe-before.png`,
+        img2: `../images/recipe-images/recipe-after.png`       
     },
     artistPortfolio : {
         name: `Artist Portfolio` ,
+        link: ``,
         text: `An artist template page created from napkin sketches, a mockup, HTML and CSS. I made two designs before settling on a center header layout that is popular amongst artists`,
         process: `💡 Process: Mood board > Napkin Sketches > Mockup > Code > Validate > Launch`, 
     }
