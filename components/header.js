4f50cf8219d396dc190da321353ad344cdd1f35b
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2022-06-11 12:38:06 -0700

    update nav and footer links

--- components/header.js
+++ components/header.js
@@ -7,22 +7,22 @@ class Header extends HTMLElement {
       this.innerHTML = `
         <ul class="navbar-nav">
       
-            <li class="nav-item px-1">
+            <li class="nav-item">
             <a class="nav-link" title="About page" href="/about.html">about</a>
             </li>
-            <li class="nav-item px-1">
+            <li class="nav-item">
             <a class="nav-link" title="Work page" href="/work.html">work</a>
             </li>
-            <li class="nav-item px-1">
+            <li class="nav-item">
               <a class="nav-link" title="dribbble page" href="https://dribbble.com/tiffanyyylizzz" target="_blank"><i class="fa-brands fa-dribbble-square"></i></a>
             </li>
-            <li class="nav-item px-1">
+            <li class="nav-item">
             <a class="nav-link" title="Linked In Link" href="https://www.linkedin.com/in/tiffanylmackay/" target="_blank"><i class="fab fa-linkedin"></i></a>
             </li>
-            <li class="nav-item px-1">
+            <li class="nav-item">
             <a class="nav-link" title="Instagram Link" href="https://www.instagram.com/tiffanyyylizzz/" target="_blank"><i class="fab fa-instagram"></i></a>
             </li>
-            <li class="nav-item px-1">
+            <li class="nav-item">
             <a class="nav-link" title="Github Link" href="https://github.com/tiffanymackay" target="_blank"><i class="fab fa-github-square"></i></a>
             </li>
         </ul> 
