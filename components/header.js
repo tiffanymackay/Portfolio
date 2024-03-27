470512b21a6c173fd0114d67c711ba50949e729d
Tiffany Mackay <88056784+tiffanymackay@users.noreply.github.com>
2024-03-26 20:33:29 -0700

    update to case studies

--- components/header.js
+++ components/header.js
@@ -8,13 +8,13 @@ class Header extends HTMLElement {
         <ul class="navbar-nav">
       
             <li class="nav-item">
-            <a class="nav-link" title="Coding page" href="/coding.html">coding</a>
+            <a class="nav-link" title="Coding page" href="/coding.html">Coding</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" title="Dribbble design" href="/work.html">design</a>
+            <a class="nav-link" title="Dribbble design" href="/work.html">Design</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" title="About page" href="/about.html">about</a>
+            <a class="nav-link" title="About page" href="/about.html">About</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" title="dribbble page" href="https://dribbble.com/tiffanyyylizzz" target="_blank"><i class="fa-brands fa-dribbble-square"></i></a>
